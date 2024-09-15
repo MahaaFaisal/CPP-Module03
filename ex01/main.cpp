9219db883ae242721df05b44af58453ab2735135
mahaafaisal <mahafaisalmubarak@gmail.com>
2024-09-15 13:54:43 +0400

    ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,8 +4,16 @@
 int	main()
 {
 	ScavTrap st1;
+	std::cout << std::endl;
+
 	ScavTrap st2("Scavvy");
+	std::cout << std::endl;
+
 	ScavTrap st3(st2);
+	std::cout << std::endl;
+
+	ScavTrap st4 = st3;
+	std::cout << std::endl;
 
 	st1.attack("someone");
 	st2.attack("another person");
